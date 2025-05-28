ddeddf41ef46cf16029bdc4af32021131f67e1d8
William Couture <wcouture17@gmail.com>
2025-05-28 13:11:35 -0400

    Begins server implementation

--- app/utils/socket.js
+++ app/utils/socket.js
@@ -1,3 +1,10 @@
 import { io } from "socket.io-client";
-const socket = io.connect("http://localhost:4000");
+const socket = io.connect("ws://apps.willc-dev.net");
+
+socket.on("hello", (arg) => {
+  console.log(arg);
+});
+
+socket.emit("howdy", "stranger");
+
 export default socket;
--- server/index.js
+++ server/index.js
@@ -1,37 +1,8 @@
-const express = require("express");
-const app = express();
-const PORT = 4001;
+import { Server } from "socket.io";
+const PORT = 3000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const io = new Server(PORT);
 
-const http = require("http").Server(app);
-const cors = require("cors");
-
-app.use(cors());
-
-const socketIO = require("socket.io")(http, {
-  cors: {
-    origin: "<http://localhost:3000>",
-  },
-});
-
-//👇🏻 Add this before the app.get() block
-socketIO.on("connection", (socket) => {
-  console.log(`⚡: ${socket.id} user just connected!`);
-
-  socket.on("disconnect", () => {
-    socket.disconnect();
-    console.log("🔥: A user disconnected");
-  });
-});
-
-app.get("/api", (req, res) => {
-  res.json({
-    message: "Hello world",
-  });
-});
-
-http.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+io.on("connection", (socket) => {
+  socket.emit("connected", true);
 });
