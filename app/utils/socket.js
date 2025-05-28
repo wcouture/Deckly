600cb810144148613047d4f5c642458db8137287
William Couture <wcouture17@gmail.com>
2025-05-28 13:17:40 -0400

    Prints response to connected event on client

--- app/utils/socket.js
+++ app/utils/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 const socket = io.connect("ws://apps.willc-dev.net");
 
-socket.on("hello", (arg) => {
+socket.on("connected", (arg) => {
   console.log(arg);
 });
 
