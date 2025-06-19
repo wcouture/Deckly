ed576b33e05ff4dc10511deb87969853ccf64cb1
William Couture <wcouture17@gmail.com>
2025-06-19 15:50:04 -0400

    Changes connection url back to apps.willc-dev.net

--- app/utils/socket.js
+++ app/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-export const socket = io.connect("http://localhost:3000", {
+export const socket = io.connect("http://apps.willc-dev.net", {
   transports: ["websocket"],
 });
 
